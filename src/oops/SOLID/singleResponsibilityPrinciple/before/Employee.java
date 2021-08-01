909a5d723b9ed1f94c52b898e66d421b52bec36f
paramveersoundane <paramveersoundane@gmail.com>
2021-08-01 17:07:03 +0530

    First Commit

--- src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
+++ src/oops/SOLID/singleResponsibilityPrinciple/before/Employee.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+// starting 
 /*
 Models an employee form a business perspective
  */
